26835c5c87caf376e12f21b745ed2e9675c4e17b
Emily Wilson <emilywilson6263@gmail.com>
2020-08-12 12:17:39 -0400

    fix issues for heroku build

--- pages/components/studentListItem.js
+++ pages/components/studentListItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const StudentListItem = ({ code }) => (
-  <div>
-    <img src={code.qrCode} alt={code.id} />
-    <p>
-      {`${code.first} ${code.last}`}
-    </p>
-  </div>
-);
+const StudentListItem = ({ code }) => {
+  if (code) {
+    return (
+      <div>
+        <img src={code.qrCode} alt={code.id} />
+        <p>
+          {`${code.first} ${code.last}`}
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div>Loading</div>
+  );
+};
 
 StudentListItem.propTypes = {
   code: PropTypes.shape({
